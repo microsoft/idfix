848deb75fd8dd56e053c00bba0690bc86cc7a68f
bryan-phillips-ms <77496304+bryan-phillips-ms@users.noreply.github.com>
2022-01-25 17:09:25 -0500

    Fixed empty domains.txt file in ClickOnce app (#71)
    Co-authored-by: Bryan Phillips <bphillips76@hotmail.com>

--- src/IdFix/StringLiterals.Designer.cs
+++ src/IdFix/StringLiterals.Designer.cs
@@ -19,7 +19,7 @@ namespace IdFix {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class StringLiterals {
@@ -340,7 +340,7 @@ namespace IdFix {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to domains.txt does not exist at {0}.  Reverting to use embedded resources defaultdomains.txt..
+        ///   Looks up a localized string similar to domains.txt does not exist at {0}.  Reverting to use embedded resource domains.txt..
         /// </summary>
         internal static string DomainsFileNotFoundMessage {
             get {
